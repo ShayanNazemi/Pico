346bc2daf58fcff528c43b4b35a8720fe1c2e6e5
ShayanNazemi <nazemi.shayan@gmail.com>
2021-07-08 20:47:16 +0430

    debug

--- MACrossNotifier.py
+++ MACrossNotifier.py
@@ -1,8 +1,8 @@
-import numpy as np
+import time
 import pandas as pd
-import threading
 import requests
 from binance.client import Client
+
 
 class MACrossNotifier:
     def __init__(self, symbol='BTCUSDT'):
