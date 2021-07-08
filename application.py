b9d1ad9dd23d75e1b0499ec20b422287addfe0d6
ShayanNazemi <nazemi.shayan@gmail.com>
2021-07-09 02:07:46 +0430

    ss

--- application.py
+++ application.py
@@ -1,33 +1,27 @@
 import time
-# import threading
-# import pandas as pd
-# from MACrossNotifier import MACrossNotifier
+import threading
+import pandas as pd
+from MACrossNotifier import MACrossNotifier
 
 if __name__ == '__main__':
-    c = 100
-    x = 0
-    while x < c:
-        print('Testing ...')
-        time.sleep(2)
-        x += 1
+    btc_notifier = MACrossNotifier('BTCUSDT')
+    eth_notifier = MACrossNotifier('ETHUSDT')
 
-    # btc_notifier = MACrossNotifier('BTCUSDT')
-    # eth_notifier = MACrossNotifier('ETHUSDT')
-    #
-    # try:
-    #     last_call = pd.Timestamp(0)
-    #     while True:
-    #         m = pd.Timestamp(time.time(), unit='s').minute
-    #         s = pd.Timestamp(time.time(), unit='s').second
-    #         if m % 5 == 0 and 0 < s < 2:
-    #             if pd.Timestamp(time.time(), unit='s') - last_call > pd.Timedelta(seconds=100):
-    #                 last_call = pd.Timestamp(time.time(), unit='s')
-    #                 t_btc = threading.Thread(target=btc_notifier.signal)
-    #                 t_eth = threading.Thread(target=eth_notifier.signal)
-    #
-    #                 t_btc.start()
-    #                 t_eth.start()
-    #         time.sleep(1)
-    #
-    # except Exception as e:
-    #     print('Agent Stopped!')
+    try:
+        last_call = pd.Timestamp(0)
+        while True:
+            t = pd.Timestamp(time.time(), unit='s')
+            m, s = t.minute, t.second
+            if m % 5 == 0 and 0 < s < 2:
+                print(f"({t}) Looping ...")
+                if pd.Timestamp(time.time(), unit='s') - last_call > pd.Timedelta(seconds=100):
+                    last_call = pd.Timestamp(time.time(), unit='s')
+                    t_btc = threading.Thread(target=btc_notifier.signal)
+                    t_eth = threading.Thread(target=eth_notifier.signal)
+
+                    t_btc.start()
+                    t_eth.start()
+            time.sleep(1)
+
+    except Exception as e:
+        print('Agent Stopped!')
