cfaced3cbd4d7ec903692d4e390edb20fb233016
ShayanNazemi <nazemi.shayan@gmail.com>
2021-07-14 14:54:11 +0430

    test worker

--- Procfile
+++ Procfile
@@ -1,3 +1,9 @@
 worker: python application.py
 
 processor: python run.py
+
+background: python run.py
+
+model: python run.py
+
+crawler: python run.py
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import time
+import numpy as np
 
 if __name__ == '__main__':
     x = 0
+    process_num = np.random.randint(0, 10000)
     while True:
         time.sleep(2)
         x += 1
-        print('X = ', x)
+        print(f'Process : {process_num} - X = {x}')
